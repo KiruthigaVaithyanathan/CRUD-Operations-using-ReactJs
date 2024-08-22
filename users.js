0e5f5361766a704f50f1fe7a19f7bd810ad617b7
Kiruthiga V <kiruthigavaithi1@gmail.com>
2024-08-22 21:43:58 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,7 @@
+const Users=[
+    {id:"1", Name:"Kiruthiga",Email:"kiru@gmail.com",MobileNumber:"9080867585",Age:22,},
+    {id:"2", Name:"Saisaran",Email:"sai@gmail.com",MobileNumber:"8610982001",Age:19},
+    {id:"3", Name:"Kovarthan",Email:"kho@gmail.com",MobileNumber:"6381329543",Age:13}
+];      //Default records
+
+export default Users;
